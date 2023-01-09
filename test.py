e66de1e631d75d2979487da5a42e0a1f67f508a1
highb33kay <hiibeekayvibe@gmail.com>
2023-01-09 16:03:52 +0100

    test.py

---
+++ test.py
@@ -0,0 +1,8 @@
+test_type = input("Enter a variable: ")
+print(test_type)
+if test_type.isdigit():
+    test_type = int(test_type)
+    print(type(test_type))
+else:
+    print(type(test_type))
+# 78
